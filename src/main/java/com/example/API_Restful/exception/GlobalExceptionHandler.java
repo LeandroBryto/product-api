78b4739f31e9834e9f21bdc678ae240241e6d15d
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-01-15 20:17:28 -0300

    Update GlobalExceptionHandle

--- src/main/java/com/example/API_Restful/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/API_Restful/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.example.API_Restful.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityFoundException(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity.badRequest().body("Argumenta inválido:" + e.getMessage());
+    }
 }
