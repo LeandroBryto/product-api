4e3d8dd41d694e20e721af8b55d079c22d9d7e77
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-01-15 20:53:39 -0300

    Update ExceptionHandle service

--- src/main/java/com/example/API_Restful/service/ProductService.java
+++ src/main/java/com/example/API_Restful/service/ProductService.java
@@ -2,6 +2,7 @@ package com.example.API_Restful.service;
 
 import com.example.API_Restful.model.ProductModel;
 import com.example.API_Restful.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,9 +19,16 @@ public class ProductService {
     return productRepository.findAll();
     }
     public ProductModel getProductModelById(Long id){
-        return productRepository.findById(id).orElse(null);
+        return productRepository.findById(id).orElseThrow(() ->
+              new  EntityNotFoundException("Usuário com ID " +  id + "não encontrado"));
     }
     public ProductModel saveProductModel(ProductModel productModel){
+        if (productModel.getNome() == null || productModel.getNome().isEmpty()){
+            throw new IllegalArgumentException("O nome do usuário é obrigatório.");
+        }
+        if (productModel.getPrice() == null || productModel.getPrice() <= 0){
+            throw  new IllegalArgumentException("o preço  deve ser maior que zero.");
+        }
         return productRepository.save(productModel);
     }
     public void deleteProductModel(Long id){
